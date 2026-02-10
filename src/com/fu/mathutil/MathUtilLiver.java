daecf7fdc36dfa4fa77b281a8a7ce5c2f665fd51
vu-duan <vuduan14102003@gmail.com>
2026-02-10 23:46:51 +0700

    Lan thu 4 lam chuyen ay-Dua code len Server

--- src/com/fu/mathutil/MathUtilLiver.java
+++ src/com/fu/mathutil/MathUtilLiver.java
@@ -17,6 +17,8 @@ public class MathUtilLiver {
         System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
         System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
         System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
+        //Thêm code sau lần đầu thứ 4 chơi với git-Đưa code lên Server  11:45pm(10/2/2026)
+        System.out.println("expected: 4! = 24; actual: " + MathUtil.getFactorial(4));
     }
     
 }
